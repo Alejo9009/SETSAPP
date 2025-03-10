5315a77bf82da674ac52b12e88d001dcf7173c76
Alejo9009 <apirazanesquivel@gmail.com>
2025-03-09 21:01:49 -0500

    Delete .gitattributes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Image } from 'react-native';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+return (
+<View style={styles.container}>
+<Image
+source={ require('./assets/c46b71490cb2abe51cbe10e6fd48e19a.jpg')}
+style={styles.imageBackground}
+/>
 
+</View>
+);
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+flex: 1,
+backgroundColor: '#fff',
+},
+imageBackground: {
+width: '100%',
+height: '100%',
+},
 });
